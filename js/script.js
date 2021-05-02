84d7f3166bc516df870415c79e93cfee60a7e6df
Michal Dymek <m.dymek95@gmail.com>
2021-05-02 12:49:48 +0200

    first function

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function titleClickHandler(){
+    console.log(event);
+
+    // remove class 'active' from all articles
+    // add class 'active' to the clicked link
+    //remove class 'active' from all articles
+    //get 'href' attribute from the clicked link
+    // find the correct article using the selector (value od 'href' attribute)
+    // add class 'active' to the correct article
+  }
+  
+  const links = document.querySelectorAll('.titles a');
+  
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
