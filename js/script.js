2bb7b3a134f5c720a67255d83a4b1ad7c1276133
Michal Dymek <m.dymek95@gmail.com>
2021-05-03 16:01:03 +0200

    finish function titleClickHandler

--- js/script.js
+++ js/script.js
@@ -24,9 +24,20 @@ function titleClickHandler(event){
     for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
     }
-    //get 'href' attribute from the clicked link
+    // [DONE] get 'href' attribute from the clicked link
+    
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector)
+
     // find the correct article using the selector (value od 'href' attribute)
+    
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
     // add class 'active' to the correct article
+
+    targetArticle.classList.add('active');
+
   }
   
   const links = document.querySelectorAll('.titles a');
